ea1b22a74011423b779b384ef68245c54b603b5c
Crowdbotics <team@crowdbotics.com>
2023-07-05 15:23:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-fog-41919",
+  url: "https://fragrant-fog-41919.botics.co",
+  api: "https://fragrant-fog-41919.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
